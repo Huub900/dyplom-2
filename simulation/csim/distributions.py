df5e2869b67a68e75d465f56bb3098d953642f98
Hubert Czobodziński <hcz@onet.eu>
2010-07-06 00:33:29 +0200

    Rozkład Weibulla -- symulacja i odwrotność dystrybuanty

--- simulation/csim/distributions.py
+++ simulation/csim/distributions.py
@@ -1,6 +1,22 @@
-class Weibull(object):
-    def __init__(lambda, k):
-        self.lambda = lambda
-        self.k = k
+# -*- coding: utf-8 -*-
 
-    def revcdf(
+import math
+from random import uniform
+
+
+class Weibull(object):
+    def __init__(self, l, k):
+        self.l = float(l)
+        self.k = float(k)
+
+    def revcdf(self, x):
+        return math.log(1 / (1 - x)) ** (1 / self.k) / self.l
+
+    def sample(self):
+        return self.revcdf(uniform(0, 1))
+
+
+if __name__ == '__main__':
+    w = Weibull(1, 1.5)
+    for i in range(500):
+        print(w.sample())
