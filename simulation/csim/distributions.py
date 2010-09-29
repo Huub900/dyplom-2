eddbaca935a65e73740af0cf36b4fc412cb14a1a
Hubert Czobodziński <hcz@onet.eu>
2010-09-29 15:25:26 +0200

    Rozkłady - symulacje i odwracanie dystrybuanty.

--- simulation/csim/distributions.py
+++ simulation/csim/distributions.py
@@ -4,10 +4,6 @@ import math
 from random import uniform
 from scipy import stats
 
-# TODO: nie wiem, czy to tak dokładnie ma być
-# raczej location + scale
-# w przypadku stałego ucinania -- tylko jeden parametr
-
 class Distribution(object):
     def __init__(self):
         raise Exception("Klasa abstrakcyjna.")
@@ -20,9 +16,9 @@ class Distribution(object):
 
 
 class Weibull(Distribution):
-    def __init__(self, l, k):
-        self.l = float(l)
-        self.k = float(k)
+    def __init__(self, params):
+        self.l = float(params[0])
+        self.k = float(params[1])
 
     def revcdf(self, x):
         return math.log(1 / (1 - x)) ** (1 / self.k) / self.l
@@ -32,16 +28,32 @@ class Weibull(Distribution):
 
 
 class LogNorm(Distribution):
-    def __init__(self, *args):
-        self.dist = stats.lognorm(float(args[0]), scale=float(args[1]))
+    def __init__(self, *params):
+        self.dist = stats.lognorm(float(params[0]), scale=float(params[1]))
 
 
 class Normal(Distribution):
-    def __init__(self, *args):
-        self.dist = stats.norm(loc=float(args[0]), scale=float(args[1]))
+    def __init__(self, *params):
+        self.dist = stats.norm(loc=float(params[0]), scale=float(params[1]))
+
+
+class Uniform(Distribution):
+    def __init__(self, *params):
+        self.tmax = float(params[0])
+
+    def sample(self):
+        return uniform(0, self.tmax)
+
+
+class Constant(Distribution):
+    def __init__(self, *params):
+        self.c = params[0]
+
+    def sample(self):
+        return self.c
 
 
 if __name__ == '__main__':
-    w = Weibull(1, 1.5)
+    w = Constant(3)
     for i in range(500):
         print(w.sample())
