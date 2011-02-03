c08efef28dc9bef3ff17c0861269d97026929c38
Hubert Czobodziński <czobodzi@wit.edu.pl>
2011-02-03 21:28:40 +0100

    Nelsen2 za pomocą dystrybuanty warunkowej -- już trochę lepiej...

--- simulation/csim/copulae.py
+++ simulation/csim/copulae.py
@@ -7,14 +7,21 @@ from sys import float_info
 
 
 class CopulaI(object):
+    def __init__(self, theta):
+        self.theta = float(theta)
+
     def phi(self, t):
+        raise NotImplementedError
+
+    def dphi(self, t):
         raise NotImplementedError
 
     def revphi(self, t):
         return bisect(lambda x: self.phi(x) - t, float_info.min, 1.0)
 
     def kc(self, t):
-        raise NotImplementedError
+        print t, self.dphi(t)
+        return t - self.phi(t) / self.dphi(t)
 
     def revkc(self, t):
         return bisect(lambda x: self.kc(x) - t, float_info.min, 1.0)
@@ -23,7 +30,7 @@ class CopulaI(object):
         s, t = uniform(0, 1), uniform(0, 1)
         w = self.revkc(t)
         u = self.revphi(s * self.phi(w))
-        v = self.revphi((1 - s) * self.phi(w))
+        v = self.revphi((1.0 - s) * self.phi(w))
         return u, v
 
 
@@ -55,7 +62,7 @@ class Gumbel(CopulaI):
         return (-log(t)) ** self.theta
 
     def revphi(self, t):
-        return exp(-t ** (1 / self.theta))
+        return exp(-t ** (1.0 / self.theta))
 
     def kc(self, t):
         return t + t * log(t) / self.theta
@@ -66,43 +73,54 @@ class Clayton(CopulaII):
         self.theta = float(theta)
 
     def phi(self, t):
-        return (t ** -self.theta - 1) / self.theta
+        return (t ** -self.theta - 1.0) / self.theta
 
     def revphi(self, t):
-        return (self.theta * t + 1) ** (-1 / self.theta)
+        return (self.theta * t + 1.0) ** (-1.0 / self.theta)
 
     def dphi(self, t):
-        return -(t ** (-self.theta - 1))
+        return -(t ** (-self.theta - 1.0))
 
     def revdphi(self, t):
-        return (-t) ** (1 / (-self.theta - 1))
+        return (-t) ** (1.0 / (-self.theta - 1.0))
 
 
 class AliMikhailHaq(CopulaI):
-    def __init__(self, theta):
-        self.theta = theta
-
     def phi(self, t):
-        return log((1 - self.theta * (1 - t)) / t)
+        return log((1.0 - self.theta * (1.0 - t)) / t)
 
     def kc(self, t):
-        a = 1 - self.theta * (1 - t)
-        return t - log(a / t) * t * a / (1 + self.theta)
+        a = 1.0 - self.theta * (1.0 - t)
+        return t - log(a / t) * t * a / (1.0 + self.theta)
 
 
-class Nelsen2(object):
+class Nelsen2(CopulaI):
+    def phi(self, t):
+        return (1.0 - t) ** self.theta
+
+    def revphi(self, t):
+        return 1.0 - t ** (1.0 / self.theta)
+
+    def kc(self, t):
+        return 0.0 if t <= 1.0 else t + (1.0 - t) * self.theta
+
+    def revkc(self, t):
+        return (self.theta * t- 1.0) / (self.theta - 1.0)
+
+
+class Nelsen22(object):
     def __init__(self, theta):
-        self.theta = theta
+        self.theta = float(theta)
 
     def sample(self):
         th = self.theta
         u, w = uniform(0, 1), uniform(0, 1)
-        v = 1 - ((1 - u) ** th * (w ** (th / (1 - th)) - 1) + 1) ** (1 / th)
+        v = 1.0 - ((w ** (th / (1.0 - th)) - 1.0) * (1.0 - u) ** th) ** (1.0 / th)
         return u, v
 
 
 if __name__ == '__main__':
-    copula = AliMikhailHaq(0.9)
+    copula = Nelsen22(3)
     for i in range(500):
         sample = copula.sample()
         print("%s,%s" % (sample[0], sample[1]))
